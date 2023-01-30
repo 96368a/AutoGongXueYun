45b6b795ceb317e732557cab29f8b70db3209b3a
logs404 <ncxxg@qq.com>
2023-01-30 21:27:58 +0800

    feat: 增加接口跨域~

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+origins = [
+    "http://localhost:5173",
+    "http://localhost:4000",
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 # 初始化路由
 from app import router
 
